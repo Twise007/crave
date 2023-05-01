8e36584a8f493ae9cdbe9708c82eb19ff2ab6e75
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-05-01 13:58:07 +0100

    staff

--- src/pages/Chefs.jsx
+++ src/pages/Chefs.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../styles";
+import { popularMeal } from "../constant";
 
 const Chefs = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="text-center">
+      <p className={`${styles.sectionSubText}`}>Our Staff</p>
+      <div className="border-t-8 ">
+        <div className="flex flex-wrap items-center justify-center gap-10 mt-10 ">
+          {popularMeal.map((meal, index) => (
+            <div className="p-4 pt-2 card w-[280px] border-2 rounded-none">
+              <figure className="flex flex-col border-b-2 ">
+                <img
+                  src={meal.image}
+                  alt="meal"
+                  className="object-cover w-[200px] h-[200px]"
+                />
+                <h1 className="uppercase">JoHN DOE</h1>
+              </figure>
+              <div className={`items-center justify-center text-center`}>
+                <h1 className="pt-2 capitalize">{meal.title}</h1>
+                <div className="flex items-center pt-2 justify-evenly">
+                  <div className={`${styles.btnRed}`}></div>
+                  <div className={`${styles.btnRed}`}></div>
+                  <div className={`${styles.btnRed}`}></div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chefs
+export default Chefs;
--- src/pages/Testimonies.jsx
+++ src/pages/Testimonies.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Testimonies = () => {
   return (
-    <div>Testimonies</div>
-  )
+    <div className="shadow-xl card card-side bg-base-100">
+      <figure>
+        <img
+          src=""
+          alt="testify"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div className="justify-end card-actions">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonies
