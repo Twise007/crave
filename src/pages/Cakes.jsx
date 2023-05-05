1ec1a99277b00ced6bedc83ed03993522468fdd6
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-05-05 17:24:49 +0100

    sidebar

--- src/pages/Cakes.jsx
+++ src/pages/Cakes.jsx
@@ -1,14 +1,11 @@
 import { styles } from "../styles";
 import { coffee } from "../assets";
+import{MdDeliveryDining} from "react-icons/md"
 
 const Cakes = () => {
   return (
     <div>
-      <p
-        className={`${styles.sectionSubText} text-end text-bg-black `}
-      >
-        Cakes
-      </p>
+      <p className={`${styles.sideBarTitle}`}>cakes</p>
       <div className="h-full hero">
         <div className="flex-col hero-content lg:flex-row-reverse">
           <img
@@ -16,7 +13,11 @@ const Cakes = () => {
             alt="meal"
             className="ronded-lg w-[8rem] md:w-[24rem]"
           />
-          <div className="">
+          <div>
+            <p className="flex items-center justify-center gap-1 p-2 mb-1 leading-4 text-black duration-500 border rounded-full cursor-pointer bg-bg-white border-bg-black hover:bg-bg-black hover:text-bg-white">
+              Faster Food Service{" "}
+              <MdDeliveryDining color="red" className="text-3xl" />
+            </p>
             <h1
               className={`${styles.sectionHeadText} text-bg-red leading-none`}
             >
--- src/pages/Coffee.jsx
+++ src/pages/Coffee.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { styles } from "../styles";
 import { coffee } from "../assets";
+import { MdDeliveryDining } from "react-icons/md";
 
 const Coffee = () => {
   return (
-    <div className="h-full hero">
-      <div className="flex-col hero-content lg:flex-row-reverse">
-        <img
-          src={coffee}
-          alt="meal"
-          className="ronded-lg w-[8rem] md:w-[24rem]"
-        />
-        <div className="">
-          <h1 className={`${styles.sectionHeadText} text-bg-red leading-none`}>
-            get delivered while it`s hot.
-          </h1>
-          <p className="py-6 text-bg-black">
-            Bring together your discussions membership and content. <br />
-            Start your 7-day free trail
-          </p>
-          <button className={`${styles.btnBlack}`}>Get Started</button>
+    <div>
+      <p className={`${styles.sideBarTitle}`}>coffee</p>
+      <div className="h-full hero">
+        <div className="flex-col hero-content lg:flex-row-reverse">
+          <img
+            src={coffee}
+            alt="meal"
+            className="ronded-lg w-[8rem] md:w-[24rem]"
+          />
+          <div>
+            <p className="flex items-center justify-center gap-1 p-2 mb-1 leading-4 text-black duration-500 border rounded-full cursor-pointer bg-bg-white border-bg-black hover:bg-bg-black hover:text-bg-white">
+              Faster Food Service
+              <MdDeliveryDining color="red" className="text-3xl" />
+            </p>
+            <h1
+              className={`${styles.sectionHeadText} text-bg-red leading-none`}
+            >
+              get delivered while it`s hot.
+            </h1>
+            <p className="py-6 text-bg-black">
+              Bring together your discussions membership and content. <br />
+              Start your 7-day free trail
+            </p>
+            <button className={`${styles.btnBlack}`}>Get Started</button>
+          </div>
         </div>
       </div>
     </div>
