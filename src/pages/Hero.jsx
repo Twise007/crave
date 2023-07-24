22844ad2e3028cc7b0a75f2fdeb3fcc9271fbafc
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-07-24 22:14:22 +0100

    hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
     >
       {heroBanner.map((banner, index) => (
         <SwiperSlide>
-          <div className="hero">
+          <div className="max-h-screen hero">
             <div className="flex-col pt-20 hero-content lg:flex-row-reverse">
               <img
                 src={banner.image}
